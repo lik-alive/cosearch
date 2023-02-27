efdf8caa0200c353ecca2aaef2504a6ad70ee281
lik-alive <kirshdv@gmail.com>
2023-02-28 02:30:09 +0400

    unify deployment

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$1" == "--prod" ]; then
   echo "Start prod server"
-  docker-compose up -d --remove-orphans --build
+  env MODE=production docker-compose up -d --remove-orphans --build
 else 
   echo "Start dev server"
-  docker-compose up --build
+  env MODE=development docker-compose up --build
 fi
